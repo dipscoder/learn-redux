f43212d8cbcac2220b54260196d9e8369419a140
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-04 21:55:01 +0530

    Add reducer

--- src/index.js
+++ src/index.js
@@ -18,6 +18,16 @@ const decrement = () => {
   }
 }
 
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+
+    case "DECREMENT":
+      return state - 1;
+  }
+}
 
 
 
