69585295117155fce24796ca93f17e54db4cde56
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-05 17:04:19 +0530

    Action with argument

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>Counter: {counter} </h1>
-      <button onClick={() => dispatch(increment())} >+</button>
+      <button onClick={() => dispatch(increment(5))} >+</button>
       <button onClick={() => dispatch(decrement())} >-</button>
       {isLogged ? <h3>Only seen when you are logged in</h3> : '' }
     </div>
